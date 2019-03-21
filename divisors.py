2632586b39b72324427543a7dac97314686264f5
Your Name <ksaitoireland@gmail.com>
2019-03-21 04:52:54 +0000

    divisors.py is modified

--- divisors.py
+++ divisors.py
@@ -1,6 +1,6 @@
-for x in range(1000, 10000):
-  if x%6==0:
-   print (str(x)+ " ", end = "") 
+for num in range (1000,10000):
 
-  else: 
-       pass
+  if num % 6 == 0 and not num % 12 == 0:
+    
+    print (num)
+
