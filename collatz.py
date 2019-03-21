881fd6818912f0e8359f09f7c9970c24563d9b69
Your Name <ksaitoireland@gmail.com>
2019-03-21 12:18:50 +0000

    first attempt on collatz.py

--- collatz.py
+++ collatz.py
@@ -1,1 +1,13 @@
-#this is a solution for Q4
+#Kiyoko Saito 2019-3-21
+# This is solution for Q4 
+
+start = int(input("Please enter an integer: "))
+ans = 1
+
+while start % 2 == 0:
+  ans = ans/2
+  
+if start % 2 == 1:
+  ans = ans * 3 + 1
+
+print(ans)
