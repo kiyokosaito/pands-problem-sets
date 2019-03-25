bfa6f7fed26e66eb05120d0369e6f0fefb1c25d0
Your Name <ksaitoireland@gmail.com>
2019-03-25 20:35:50 +0000

    secondstring.py is completed

--- secondstring.py
+++ secondstring.py
@@ -1,2 +1,9 @@
 #this is a solution for Q6 
 
+str = input("Please enter a sentence: ")
+
+words = " ".join(words for i, words in enumerate(str.split())if not i%2)
+
+words = " ".join(str.split()[::2])
+
+print (repr(words))
