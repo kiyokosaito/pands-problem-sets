074f17d566e2d2e7ab6d69b98229eaf92a0d2771
Your Name <ksaitoireland@gmail.com>
2019-03-27 21:24:24 +0000

    squareroot.py is completed

--- determin.py
+++ determin.py
@@ -5,4 +5,7 @@ from datetime import datetime
 now = datetime.now()
 
 dt_string = now.strftime("%A, %B %d %Y at %I : %M %p ")
+
+if %d = 1, 21,31:
+    print (%d +st) 
 print (dt_string)
--- squareroot.py
+++ squareroot.py
@@ -1,1 +1,13 @@
 #this is the solution for Q7
+# Kiyoko Saito
+# adapted from Week 8 video
+
+num = int(input("Please enter a positive number: "))
+
+rootof = num
+estimate = 6.0
+
+while abs((estimate * estimate) - rootof) > 0.1 :
+  estimate -= ((estimate * estimate ) - rootof) / (2 * estimate)
+
+print (f"The square root of {rootof} is approx. {estimate}.")
